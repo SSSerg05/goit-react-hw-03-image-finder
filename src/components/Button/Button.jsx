58acf3b685568785f2724dadc9f8639c01b5f5d2
SSSerg05 <ssserg05@gmail.com>
2023-06-28 16:35:31 +0300

    fix class button

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+
+
+export default class Button extends Component {
+  static classes = {
+    hidden: "hidden",
+  }
+
+  state = {
+    selector: null,
+    name: "",
+    nameEvent: "",
+    isHidden: true,
+  } 
+  
+
+
+  // constructor({ selector, name, nameDisable, isHidden = false }) {
+  //   this.button = this.getButton(selector)
+  //   this.name = name;
+  //   this.nameDisable = nameDisable;
+
+  //   isHidden && this.hide();
+  //   // isHidden = true && this.hide() -> true && true -> this.hide()
+  //   // isHidden = true && this.hide() -> false && true -> false
+  // }
+
+  
+  getButton(selector) {
+    return document.querySelector(selector)
+  }
+
+  hide() {
+    this.button.classList.add(Buttons.classes.hidden);
+
+  }
+
+  show() {
+    this.button.classList.remove(Buttons.classes.hidden);
+  }
+
+  disable() {
+    this.button.disabled = true;
+    this.button.textContent = this.nameDisable;
+  }
+
+  enable() {
+    this.button.disabled = false;
+    this.button.textContent = this.name;
+  }
+
+}
