457223478c6b7c10c1dc21ff08dccd1c4a289730
SSSerg05 <ssserg05@gmail.com>
2023-07-06 11:41:05 +0300

    fix

--- src/components/ImageGallery/Modal/Modal.jsx
+++ src/components/ImageGallery/Modal/Modal.jsx
@@ -3,14 +3,23 @@ import { boxModal } from "./Modal.slyled";
 
 export class Modal extends Component {
   
+  state = {
+    src: "",
+    alt: ""
+  }
   
   render() {
-    <div className="overlay">
-      <boxModal className="modal">
-        <img src={src} alt={alt} />
-        {this.props.children}
+    const { src, alt } = this.state;
+    return(
+      <div className="overlay">
+        <boxModal className="modal">
+          
+          <img src={src} alt={alt} />
+
+          { this.props.children }
         
-      </boxModal>
-    </div>
+        </boxModal>
+      </div>
+    )
   };
 }
