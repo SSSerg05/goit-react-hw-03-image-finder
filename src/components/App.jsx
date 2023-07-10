583ffa32ff6d370314ef26e6e708baf6a1d3da29
SSSerg05 <ssserg05@gmail.com>
2023-07-10 18:19:42 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,7 @@ import { ImageGallery} from "./ImageGallery/ImageGallery"
 import  dataGallery from "../data/gallery.json"
 import { Modal } from "./ImageGallery/Modal/Modal";
 
+// import axios = from "axios/dist/axios.min.js"; // node
 
 const URL = 'https://pixabay.com/api/';
 const API_KEY = '36214966-0d101d8d6f502ad642532aad3';
@@ -15,6 +16,8 @@ export class App extends Component {
     imagesGallery: dataGallery.hits,
     showModal: false,
     loader: false,
+    page: 0,
+    total: 0,
   }
 
   // відкриття / закриття модалки
@@ -25,15 +28,44 @@ export class App extends Component {
   }
 
   componentDidMount() { 
-    const url = URL + '?api_key=' + API_KEY
+    const url = URL + '?key=' + API_KEY + '&q=cat';
+    console.log(url);
+    //https://pixabay.com/api/?q=cat&key=36214966-0d101d8d6f502ad642532aad3
     fetch(url)
       .then(res => res.json())
-      .then(console.log())
-      .then(res => this.setState(({ imagesGallery }) => ({
-      imagesGallery: res
-    })));
+      .then(console.log());
+  }
+      // .then(res => this.setState(({ imagesGallery }) => ({
+      //   imagesGallery: res
+      // })))
+  
+  // async getPictures() {
+    
+  //   const params = {
+  //     key: API_KEY,
+  //     q: this.searchQuery,
+  //     image_type: 'photo',
+  //     orientation: 'horizontal',
+  //     safesearch: true,
+  //     page: this.page,
+  //     per_page: this.perPage,
+  //   }
+
+  //   const { data } = await axios.get(URL, { params })
+  //   this.incrementPage();
+  //   this.total = data.totalHits;
+
+  //   return data.hits;
+  // }
+
+  incrementPage() {
+    this.page++;
   }
 
+  resetPage() { 
+    this.page = 1;
+    this.total = 0;
+  }
 
   render() {
     const { imagesGallery, showModal } = this.state; 
